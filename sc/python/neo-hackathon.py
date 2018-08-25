9d9541acc0808120890b777fe7aa10888e95e0c6
nandehu0323 <CoralGift.N.H@uec.ac.jp>
2018-08-25 21:56:43 +0900

    :watermelon: Add:data関数、vote関数

--- sc/python/neo-hackathon.py
+++ sc/python/neo-hackathon.py
@@ -1,12 +1,14 @@
 from boa.interop.System.ExecutionEngine import GetScriptContainer, GetExecutingScriptHash
-from boa.interop.Neo.Runtime import GetTrigger, CheckWitness
+from boa.interop.Neo.Runtime import GetTrigger, CheckWitness, Log, Notify
 from boa.interop.Neo.TriggerType import Application, Verification
 from boa.interop.Neo.Storage import *
 from boa.interop.Neo.Transaction import Transaction, GetReferences, GetOutputs, GetUnspentCoins
 from boa.interop.Neo.Output import GetValue, GetAssetId, GetScriptHash
 from boa.interop.Neo.Blockchain import GetHeight
+from boa.builtins import *
 from nep5.token import *
 
+BOOKLIST = b'book_list'
 
 TOKENS_PER_NEO = 10 * 100000000
 
@@ -17,7 +19,6 @@ GAS_ASSET_ID = b'\xe7-(iy\xeel\xb1\xb7\xe6]\xfd\xdf\xb2\xe3\x84\x10\x0b\x8d\x14\
 OnTransfer = RegisterAction('transfer', 'addr_from', 'addr_to', 'amount')
 
 ctx = GetContext()
-
 
 def Main(operation, args):
     trigger = GetTrigger()
@@ -36,6 +37,28 @@ def Main(operation, args):
         if operation == 'claimTokens':
             return claim_tokens(ctx)
 
+        elif operation == 'vote':
+            if len(args) == 2:
+                return vote(ctx, args[0], args[1])
+
+        elif operation == 'getVote':
+            if len(args) == 1:
+                return get_vote(ctx, args[0])
+
+        elif operation == 'getData':
+            return get_data(ctx)
+
+        elif operation == 'putData':
+            if len(args) == 1:
+                return put_data(ctx, args[0])
+
+        elif operation == 'deleteData':
+            return delete_data(ctx)
+
+        elif operation == 'withdraw':
+            if len(args) == 1:
+                return withdraw(ctx, args[0])
+
         return 'unknown operation'
 
 def claim_tokens(ctx):
@@ -52,10 +75,69 @@ def claim_tokens(ctx):
 
     return True
 
+def vote(ctx, bhash, address):
+    attachments = get_asset_attachments()  # [receiver, sender, neo, gas]
+
+    if Get(ctx, concat(bhash, concat(b'-', attachments[1]))) != 0:
+        return False
+
+    put_vote(ctx, bhash, address)
+
+    Put(ctx, concat(bhash, concat(b'-', attachments[1])), 1)
+
+    return True
+
+def get_vote(ctx, bhash):
+    return Get(ctx, bhash)
+
+def put_vote(ctx, bhash, address):
+    vote_list = Get(ctx, bhash)
+    if len(vote_list) == 0:
+        Put(ctx, bhash, address)
+        return True
+    result = concat(vote_list, concat(b',', address))
+    Put(ctx, bhash, result)
+    return True
+
+def withdraw(ctx, bhash):
+    attachments = get_asset_attachments()
+    vote_list = Get(ctx, bhash)
+    vote_dict = votelist2dict(vote_list)
+    Notify(vote_dict)
+    return True
+
+def get_data(ctx):
+    return Get(ctx, BOOKLIST)
+
+
+def put_data(ctx, args):
+    book_list = Get(ctx, BOOKLIST)
+    if len(book_list) == 0:
+        Put(ctx, BOOKLIST, args[0])
+        return True
+    result = concat(book_list, concat(b',', args[0]))
+    Put(ctx, BOOKLIST, result)
+    return True
+
+def delete_data(ctx):
+    Delete(ctx, BOOKLIST)
+    return True
+
 
 # ################################################
 # Helper
 # ################################################
+
+def votelist2dict(vlist):
+    tmp = b''
+    tmp_dict = {}
+    for i in range(0, len(vlist)):
+        if vlist[i] == 44:
+            tmp_dict[tmp] += 1
+            tmp = b''
+        else:
+            tmp = concat(tmp, vlist[i])
+    return tmp_dict
 
 def get_asset_attachments():
     tx = GetScriptContainer()
