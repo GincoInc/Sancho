0df4b54a3eef09059cd334db21a05ec26bc6102a
nandehu0323 <CoralGift.N.H@uec.ac.jp>
2018-08-26 13:53:40 +0900

    :watermelon: Add:font-family

--- nuxt-project/nuxt.config.js
+++ nuxt-project/nuxt.config.js
@@ -10,7 +10,8 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Oswald:400,700' }
     ]
   },
   /*
@@ -42,4 +43,3 @@ module.exports = {
     ['nuxt-buefy', { /* buefy options */ }]
   ]
 }
-
